066e4b1a7a8e55bc2ec0d9df4b01da84090b2c98
Bilal Ekrem Harmansa <bilalekremharmansa@hotmail.com>
2018-10-05 23:21:29 +0300

    feat: service return HTML content of a given page

--- routes/sss.js
+++ routes/sss.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+var url = require('url');
+var htmlparser = require("htmlparser2");
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  res.send("sss");
+router.get('/', function(req, res, next) {
+  let requestedURL = req.query.url;
+  console.log("The URL is fetching..");
+  console.log(requestedURL);
+
+  axios({
+    method: "GET",
+    url: requestedURL,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    responseType:'text'
+  }).then((response) => {
+    res.set('Content-Type', 'text/html');
+    res.send(response.data);
+
+  }).catch((err) => {
+    console.log("This is console error checker log.");
+    console.log(err);
+    res.set("Content-Type", "text/json").status(402).send(err);
+  });
+
+
 });
 
 module.exports = router;
